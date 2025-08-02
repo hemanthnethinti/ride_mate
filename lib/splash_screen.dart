db0143b03cc2be1f904582ebac0be0cf42b4df3b
arshibegum <arshiyabegam@gmail.com>
2025-08-02 13:59:40 +0530

    added on-bording screens

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:ride_mate/home_page.dart';
+import 'package:ride_mate/on_boarding.dart';
 import 'package:ride_mate/wrapper.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -27,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen>
     Future.delayed(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (_) => Wrapper()),
+        MaterialPageRoute(builder: (_) => OnBoarding()),
       );
     });
   }
